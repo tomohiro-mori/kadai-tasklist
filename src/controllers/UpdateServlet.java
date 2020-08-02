ab1db59a9b2361e715dc0f6d05c7f981e12f0653
tomohiro.mori <tomohiro.mori.hs@gmail.com>
2020-08-02 18:30:33 +0900

    Add UpdateServlet

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -14,16 +14,16 @@ import models.tasks;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class CreateServlet
+ * Servlet implementation class UpdateServlet
  */
-@WebServlet("/create")
-public class CreateServlet extends HttpServlet {
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CreateServlet() {
+    public UpdateServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,28 +32,29 @@ public class CreateServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // CSRF対策
+     // CSRF対策
         String _token = (String)request.getParameter("_token");
         if (_token != null &&  _token.equals(request.getSession().getId())){
 
             // DB接続
             EntityManager em = DBUtil.createEntityManager();
 
-            // DBに新規レコード追加
-            tasks t = new tasks();
+            // DBの各カラムに値セット
+            tasks t = em.find(tasks.class, (Integer)request.getSession().getAttribute("task_id"));
 
             String content = request.getParameter("content"); // タスク内容
             t.setContent(content);
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            t.setCreated_at(currentTime);
-            t.setUpdated_at(currentTime);
+            t.setUpdated_at(currentTime); // 更新日時のみ上書き
 
             // DBトランザクション
             em.getTransaction().begin();
-            em.persist(t); // DB新規追加
             em.getTransaction().commit();
             em.close();
+
+            // セッションスコープの値を削除
+            request.getSession().removeAttribute("task_id");
 
             // indexページにリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
